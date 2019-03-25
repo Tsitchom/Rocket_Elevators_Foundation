43542720625b0e5fffce0538501cfcfd537fe06b
maximepatry <maxime.patry.01@gmail.com>
2019-03-25 15:14:03 -0400

    seeding is okay now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :validatable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :validatable
+         :recoverable
   def name
     id 
   end 
--- db/migrate/20190312135120_create_columns.rb
+++ db/migrate/20190312135120_create_columns.rb
@@ -1,7 +1,7 @@
 class CreateColumns < ActiveRecord::Migration[5.2]
   def change
     create_table :columns do |t|
-      t.references :battery, foreign_key: {on_delete: :cascade, on_update: :cascade}, null: false
+      t.belongs_to :battery, index: true, foreign_key: {on_delete: :cascade, on_update: :cascade}, null: false
       t.string :building_type, null: false
       t.integer :number_of_floors, null: false
       t.string :status
