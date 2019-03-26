93fd781a1c5df2157c66558b2a5d61a943af0fbb
maximepatry <maxime.patry.01@gmail.com>
2019-03-26 09:18:01 -0400

    some minor modifs, such as new quotes seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_165821) do
+ActiveRecord::Schema.define(version: 2019_03_26_124729) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address_type", null: false
