e06a4f8d93b5d518fe496e03799f1def6102ab7d
sdelpit <severine.delpit@gmail.com>
2019-03-27 17:07:36 -0400

    Zendesk patched

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -30,9 +30,10 @@ class PagesController < ApplicationController
 
 				#Then I want to take that info from DB and parse it into a JSON for my maps API ... 
 				@full_add = address.number_street + " " + address.city + " " + address.postal_code
-				@response = JSON.parse(Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@full_add}&key=#{ENV["Google_API"]}").body)
+				@response = JSON.parse(Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@full_add}&key=#{ENV["GOOGLE_API"]}").body)
 				
-			
+			pp address
+pp  @response
 
 				lat = @response["results"][0]["geometry"]["location"]["lat"]
 				lng = @response["results"][0]["geometry"]["location"]["lng"]
