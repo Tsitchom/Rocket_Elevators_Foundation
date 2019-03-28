95382dd1ff52e6fff2df87b8010cc290e132ba72
Tsitchom <raphael.vallee28@gmail.com>
2019-03-28 13:47:41 -0400

    Jeudi PM push before deploy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,28 +26,31 @@ class PagesController < ApplicationController
 			column_ids = Column.where(battery_id: building.battery_ids).ids
 			elevators = Elevator.where(column_id: column_ids).count
 			contact = building.customer.full_name_service_person
+			#number_of_floors = building.batteries.columns.number_of_floors
+
+				building.batteries.all.each do |battery|
+					number_of_floors = battery.columns.first.number_of_floors
+			
+				if !address.latitude		
 
 			
-			if !address.latitude		
-
-				#Then I want to take that info from DB and parse it into a JSON for my maps API ... 
-				@full_add = address.number_street + " " + address.city + " " + address.postal_code
-				@response = JSON.parse(Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@full_add}&key=#{ENV["GOOGLE_API"]}").body)
+					@full_add = address.number_street + " " + address.city + " " + address.postal_code
+					@response = JSON.parse(Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@full_add}&key=#{ENV["GOOGLE_API"]}").body)
 				
-			pp address
-			pp  @response
+				pp address
+				pp  @response
 
-				lat = @response["results"][0]["geometry"]["location"]["lat"]
-				lng = @response["results"][0]["geometry"]["location"]["lng"]
+					lat = @response["results"][0]["geometry"]["location"]["lat"]
+					lng = @response["results"][0]["geometry"]["location"]["lng"]
 				
-				address.latitude = lat
-				address.longitude = lng
-				address.save!
-				# sleep(2)
+					address.latitude = lat
+					address.longitude = lng
+					address.save!
+					# sleep(2)
+				end
+			
+				@markers << {address: address.display_address, lat: address.latitude, lng: address.longitude, customer: customer, batteries: batteries, columns: columns, elevators: elevators, contact: contact, number_of_floors: number_of_floors }
 			end
-			
-			@markers << {address: address.display_address, lat: address.latitude, lng: address.longitude, customer: customer, batteries: batteries, columns: columns, elevators: elevators, contact: contact }
 		end
 	end
-end
-
+end 
