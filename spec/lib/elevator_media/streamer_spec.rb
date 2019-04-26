fdf1c0bcbef01e2f83fd92e0900e4baeae92251b
Tsitchom <raphael.vallee28@gmail.com>
2019-04-25 20:49:16 -0400

    added more depth to tests & new module

--- spec/lib/elevator_media/streamer_spec.rb
+++ spec/lib/elevator_media/streamer_spec.rb
@@ -5,15 +5,15 @@ require 'rest-client'
 require 'rspotify'
   
 
-describe 'Streamer' do
+describe ElevatorMedia::Streamer do
     
-    let!(:streamer){Streamer.new}
+    let!(:streamer){ElevatorMedia::Streamer.new}
 
-    it "a basic passing test to set-up environment" do
+    it "a first test to initialize environment" do
         expect(true).to be true
     end
 
-    it "should get a response from getContent" do
+    it "should receive a response from getContent" do
         expect(streamer).to respond_to(:getContent)        
     end
 
@@ -24,11 +24,11 @@ describe 'Streamer' do
         it "should return a valid html" do    
             result = streamer.getContent()
             expect(result).to be_a(String)
-            expect(result).to include('<')
+            expect(result).to include('</div>')
         end
 
-        # Testing if the getHtmlFromCloud method returns html content
-        it "should get interesting html from internet" do
+        # Testing if the getHtmlFromCloud method returns "interesting" html content
+        it "should get interesting html content from internet" do
            expect(streamer).to receive(:getHtmlFromCloud)
            streamer.getContent()
         end
@@ -57,18 +57,20 @@ describe 'Streamer' do
     it 'got open-weather response' do
         current_weather = streamer.getWeather['weather'][0]['main']
         expect(current_weather).to_not eq(nil)
+        expect(current_weather).to be_a(String)
     end
 
     # Testing if asking for a weather forecast gives back a response
     it 'got open-weather forecast' do
         current_forecast = streamer.getForecast['weather'][0]['main']
         expect(current_forecast).to_not eq(nil)
+        expect(current_forecast).to be_a(String)
     end
 
     # Testing if Spotify gives back any response
     it 'got spotify response' do
         expect(RSpotify).to receive(:raw_response) {true}
-        streamer_object = Streamer.new
+        streamer_object = ElevatorMedia::Streamer.new
         valid_response = streamer_object.getSpotifyResponse
         expect(valid_response).to eq(true)
     end    
@@ -76,7 +78,7 @@ describe 'Streamer' do
     # Testing if the Spotify user is logged-in (should be true, because a client ID and secret was suppliedx)
     it 'got spotify login status' do
         expect(RSpotify).to receive(:authenticate) {true}
-        streamer_object = Streamer.new
+        streamer_object = ElevatorMedia::Streamer.new
         login_status = streamer_object.getSpotifyStatus
         expect(login_status).to eq(true)
     end
@@ -84,7 +86,7 @@ describe 'Streamer' do
     # Testing if a specific Spotify user can be found
     it 'got spotify valid user' do
         expect(RSpotify::User).to receive(:find) {"awoggddbv0ucdh3b1w86p4wmu"}
-        streamer_object = Streamer.new
+        streamer_object = ElevatorMedia::Streamer.new
         valid_user = streamer_object.getSpotifyUser
         expect(valid_user).to eq("awoggddbv0ucdh3b1w86p4wmu")
     end
