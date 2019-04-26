c17831bd3a9ccc8123b956cab349742e4460db9a
Tsitchom <raphael.vallee28@gmail.com>
2019-04-25 21:39:48 -0400

    started new test in stats controller (Bonus)

---
+++ spec/controllers/stats_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+require 'spec_helper'
+
+'/mnt/c/Users/rapha/Desktop/Rocket_Elevators_Foundation/app/controllers/interventions_controller.rb'
+
+RSpec.describe StatsController do 
+    
+    it"should give back redirect response" do
+        expect(response.status).to eq (200)
+    end
+end
