e08d93a458aac30002e7a39cc3071b38e756e397
Tsitchom <raphael.vallee28@gmail.com>
2019-05-14 15:25:09 -0400

    Deploy failed, changed user to Ubuntu, trying stuff

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "18.223.24.138", user: "rapha", roles: %w{app db web}
+server "18.223.24.138", user: "ubuntu", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
